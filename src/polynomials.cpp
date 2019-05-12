9fc0041ac6bb5ac6df4e19e6034e71468fc1beb8
Aleksejs Popovs <aleksejs@popovs.lv>
2019-05-12 00:12:09 -0400

    Implement polynomial interpolation

--- src/polynomials.cpp
+++ src/polynomials.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+
 #include "polynomials.h"
 
 uint64_t modexp(uint64_t base, uint64_t exponent, uint64_t modulus) {
@@ -10,6 +12,10 @@ uint64_t modexp(uint64_t base, uint64_t exponent, uint64_t modulus) {
         exponent = (exponent >> 1);
     }
     return result;
+}
+
+uint64_t modinv(uint64_t x, uint64_t modulus) {
+    return modexp(x, modulus - 2, modulus);
 }
 
 void polynomial_from_roots(vector<uint64_t> &roots, vector<uint64_t> &coeffs, uint64_t modulus) {
@@ -25,5 +31,50 @@ void polynomial_from_roots(vector<uint64_t> &roots, vector<uint64_t> &coeffs, ui
             coeffs[j] = (coeffs[j - 1] + neg_root * coeffs[j]) % modulus;
         }
         coeffs[0] = (coeffs[0] * neg_root) % modulus;
+    }
+}
+
+void polynomial_from_points(vector<uint64_t> &xs,
+                            vector<uint64_t> &ys,
+                            vector<uint64_t> &coeffs,
+                            uint64_t modulus)
+{
+    assert(xs.size() == ys.size());
+    coeffs.clear();
+    coeffs.resize(xs.size());
+
+    // at iteration i of the loop, basis contains the coefficients of the basis
+    // polynomial (x - xs[0]) * (x - xs[1]) * ... * (x - xs[i - 1])
+    vector<uint64_t> basis(xs.size());
+    basis[0] = 1;
+
+    // at iteration i of the loop, ddif[j] contains the divided difference
+    // [ys[j], ys[j + 1], ..., ys[j + i]]. thus initially, when i = 0,
+    // ddif[j] = [ys[j]] = ys[j]
+    vector<uint64_t> ddif = ys;
+
+    for (size_t i = 0; i < xs.size(); i++) {
+        // result += ddif[0] * basis
+        for (size_t j = 0; j < i + 1; j++) {
+            coeffs[j] = (coeffs[j] + ddif[0] * basis[j]) % modulus;
+        }
+
+        if (i < xs.size() - 1) {
+            // update basis: multiply it by (x - xs[i])
+            uint64_t neg_x = modulus - (xs[i] % modulus);
+
+            for (size_t j = i + 1; j > 0; j--) {
+                basis[j] = (basis[j - 1] + neg_x * basis[j]) % modulus;
+            }
+            basis[0] = (basis[0] * neg_x) % modulus;
+
+            // update ddif: compute length-(i + 1) divided differences
+            for (size_t j = 0; j + i + 1 < xs.size() + 1; j++) {
+                // dd_{j,j+i+1} = (dd_{j+1, j+i+1} - dd_{j, j+i}) / (x_{j+i+1} - x_j)
+                uint64_t num = (ddif[j + 1] - ddif[j] + modulus) % modulus;
+                uint64_t den = (xs[j + i + 1] - xs[j] + modulus) % modulus;
+                ddif[j] = (num * modinv(den, modulus)) % modulus;
+            }
+        }
     }
 }
--- src/polynomials.h
+++ src/polynomials.h
@@ -11,6 +11,9 @@ as vectors of coefficients.
 /* modexp(a, b, m) computes a^b mod m in O(log b) time. */
 uint64_t modexp(uint64_t base, uint64_t exponent, uint64_t modulus);
 
+/* modinv(a, m) computes a^-1 mod m in O(log m) time. */
+uint64_t modinv(uint64_t x, uint64_t modulus);
+
 /*
 polynomial_from_roots(l) computes the coefficients of the polynomial
 (x - l[0]) * (x - l[1]) * ...
@@ -18,3 +21,17 @@ polynomial_from_roots(l) computes the coefficients of the polynomial
 time complexity: O(n²), where n is the size of l
 */
 void polynomial_from_roots(vector<uint64_t> &roots, vector<uint64_t> &coeffs, uint64_t modulus);
+
+/*
+polynomial_from_points(xs, ys) computes the coefficients of a
+(xs.size() - 1)-degree polynomial f with f(xs[i]) = ys[i] for each i.
+
+if two points with the same x but different y exist, the behavior is undefined.
+if two points that are exactly the same exist, the output will still be correct.
+
+time complexity: O(n²), where n is the size of xs
+*/
+void polynomial_from_points(vector<uint64_t> &xs,
+                            vector<uint64_t> &ys,
+                            vector<uint64_t> &coeffs,
+                            uint64_t modulus);
